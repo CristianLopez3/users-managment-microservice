ad2d2328bc0bb61a83c0bd16094a675df17481af
CristianLMartinez <cristian.lm@globant.com>
2024-07-11 09:00:05 -0500

    starter project

---
+++ src/main/java/com/cristian/tiusers/TiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.tiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cristian/tiusers/TiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.tiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
